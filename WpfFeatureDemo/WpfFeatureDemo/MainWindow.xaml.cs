88d934df04d4ba4032cc3935983dea48e99cfbdb
STangella <Sree2001@gmail.com>
2016-10-13 23:58:04 -0500

    Removed default title bar. Added Custom buttons for minimize, maximize and close buttons. Made the window draggable by clicking anywhere on the application

--- WpfFeatureDemo/WpfFeatureDemo/MainWindow.xaml.cs
+++ WpfFeatureDemo/WpfFeatureDemo/MainWindow.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Input;
 
 namespace WpfFeatureDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
---
+++ WpfFeatureDemo/WpfFeatureDemo/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Prism.Commands;
+
+namespace WpfFeatureDemo
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            CloseCommand = new DelegateCommand(CloseMethod);
+            MinimizeCommand = new DelegateCommand(MinimizeMethod);
+            MaximizeCommand = new DelegateCommand(MaximizeMethod);
+        }
+
+        private void MaximizeMethod()
+        {
+            switch (Application.Current.MainWindow.WindowState)
+            {
+                case WindowState.Normal:
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                    break;
+                case WindowState.Maximized:
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                    break;
+                default:
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                    break;
+            }
+        }
+
+        private void MinimizeMethod()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseMethod()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand MinimizeCommand { get; set; }
+        public ICommand MaximizeCommand { get; set; }
+    }
+}
